f2bc0bccb4d519adeab19c0c50f82ef2abde3f09
Victor Onyekwere <cydollarsign1@gmail.com>
2025-01-14 14:04:44 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
-import Layout from "./components/layoutComponents/layout";
+import Layout from "./components/layoutComponents/Layout";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 function App() {
